ffc456b999e9d0a6d91b7a7bdc405b63d2565416
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-13 11:47:46 +0000

    PlaneTest can get plane type passed test

--- code/src/test/java/Airline/PlaneTest.java
+++ code/src/test/java/Airline/PlaneTest.java
@@ -1,12 +1,21 @@
 package Airline;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static Airline.Plane.Type.AIRBUS_A380;
+import static org.junit.Assert.assertEquals;
 
 public class PlaneTest {
     Plane plane;
 
     @Before
     public void before() {
-        plane = new Plane(Plane.Type.AIRBUS_A380);
+        plane = new Plane(AIRBUS_A380);
+    }
+
+    @Test
+    public void canGetType(){
+        assertEquals(AIRBUS_A380, plane.getType());
     }
 }
