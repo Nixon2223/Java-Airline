b04ae1e73e6f9c818c245af032125d7f931a59fe
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-13 11:52:50 +0000

    Passenger setters set

--- code/src/main/java/Airline/Passenger.java
+++ code/src/main/java/Airline/Passenger.java
@@ -1,4 +1,30 @@
 package Airline;
 
 public class Passenger {
+
+    private String name;
+    private int bags;
+
+
+    public void Passenger(String name, int bags){
+        this.name = name;
+        this.bags = bags;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBags(int bags) {
+        this.bags = bags;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBags() {
+        return bags;
+    }
+
 }
