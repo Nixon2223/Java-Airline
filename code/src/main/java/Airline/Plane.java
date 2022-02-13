f728799a4cf7e02cf6e1a436c618776e4ab9e572
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-13 11:37:00 +0000

    Plane constructor and getter and setters set

--- code/src/main/java/Airline/Plane.java
+++ code/src/main/java/Airline/Plane.java
@@ -5,6 +5,14 @@ public class Plane {
         AIRBUS_A380(200, 5000),
         BOEING_757(300, 7000);
 
+        public int getCapacity() {
+            return capacity;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
         private final int capacity;
         private final int weight;
 
@@ -12,5 +20,19 @@ public class Plane {
             this.capacity = capacity;
             this.weight = weight;
         }
+    }
+
+    private Type type;
+
+    public Plane(Type type) {
+        this.type = type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
     }
 }
