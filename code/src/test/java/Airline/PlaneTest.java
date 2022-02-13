d940fe62858acbde3c9ee34dc7fc3927fe5c23cb
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-13 11:44:42 +0000

    PlaneTest class setup and before set

--- code/src/test/java/Airline/PlaneTest.java
+++ code/src/test/java/Airline/PlaneTest.java
@@ -1,4 +1,12 @@
 package Airline;
 
+import org.junit.Before;
+
 public class PlaneTest {
+    Plane plane;
+
+    @Before
+    public void before() {
+        plane = new Plane(Plane.Type.AIRBUS_A380);
+    }
 }
