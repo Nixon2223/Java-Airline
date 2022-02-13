12fa9057ee228134981e78dbb48b11f028bf87d5
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-13 00:57:12 +0000

    Plane type enum set (capacity, weight)

--- code/src/main/java/Airline/Plane.java
+++ code/src/main/java/Airline/Plane.java
@@ -1,4 +1,16 @@
 package Airline;
 
 public class Plane {
+    public enum Type {
+        AIRBUS_A380(200, 5000),
+        BOEING_757(300, 7000);
+
+        private final int capacity;
+        private final int weight;
+
+        Type(int capacity, int weight) {
+            this.capacity = capacity;
+            this.weight = weight;
+        }
+    }
 }
