85f4386626f98c36ca8f7e9257914c294896f2f7
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-14 01:28:16 +0000

    Pilot - Test - canGetName passed

--- code/src/test/java/Airline/Employees/PilotTest.java
+++ code/src/test/java/Airline/Employees/PilotTest.java
@@ -1,4 +1,25 @@
 package Airline.Employees;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+    Pilot pilot;
+
+    @Before
+    public void before() {
+        pilot = new Pilot("Bob","Co-pilot");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Bob", pilot.getName());
+    }
+
+    @Test
+    public void canGetRank(){
+        assertEquals("Co-pilot", pilot.getRank());
+    }
 }
