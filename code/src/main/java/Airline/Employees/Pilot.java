56b74240c9e46578f567d22a80ae5f3db2c7519a
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-14 01:24:17 +0000

    Pilot class - getter set

--- code/src/main/java/Airline/Employees/Pilot.java
+++ code/src/main/java/Airline/Employees/Pilot.java
@@ -1,4 +1,14 @@
 package Airline.Employees;
 
-public class Pilot {
+public class Pilot extends Employee{
+    private String rank;
+
+    public Pilot(String name, String rank){
+        super(name);
+        this.rank = rank;
+    }
+
+    public String getRank() {
+        return rank;
+    }
 }
