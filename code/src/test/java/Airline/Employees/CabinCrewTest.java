723236cf1f5812c8c7737076aedfe6647bb22fd7
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-14 00:57:56 +0000

    CabinCrew - test - canGetName passed

--- code/src/test/java/Airline/Employees/CabinCrewTest.java
+++ code/src/test/java/Airline/Employees/CabinCrewTest.java
@@ -1,4 +1,26 @@
 package Airline.Employees;
 
+import Airline.Passenger;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CabinCrewTest {
+    CabinCrew cabinCrew;
+
+    @Before
+    public void before() {
+        cabinCrew = new CabinCrew("Ron","Trainee");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Ron", cabinCrew.getName());
+    }
+
+    @Test
+    public void canGetBags(){
+        assertEquals(2, cabinCrew.getRank());
+    }
 }
