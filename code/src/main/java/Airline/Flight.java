c6d28c1eb971ad088ac7c5af8fb014c9bea8bf91
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-14 01:49:29 +0000

    Flight - Class - setters created

--- code/src/main/java/Airline/Flight.java
+++ code/src/main/java/Airline/Flight.java
@@ -1,4 +1,83 @@
 package Airline;
 
+import Airline.Employees.CabinCrew;
+import Airline.Employees.Pilot;
+
+import java.util.ArrayList;
+
 public class Flight {
+
+    private Pilot pilot;
+    private ArrayList<CabinCrew> cabinCrew;
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNum;
+    private String departureAirport;
+    private String getDepartureTime;
+
+    public Flight(Pilot pilot, ArrayList<CabinCrew> cabinCrew, ArrayList<Passenger> passengers, Plane plane, String flightNum, String departureAirport, String getDepartureTime) {
+        this.pilot = pilot;
+        this.cabinCrew = cabinCrew;
+        this.passengers = passengers;
+        this.plane = plane;
+        this.flightNum = flightNum;
+        this.departureAirport = departureAirport;
+        this.getDepartureTime = getDepartureTime;
+    }
+
+    public void setPilot(Pilot pilot) {
+        this.pilot = pilot;
+    }
+
+    public void setCabinCrew(ArrayList<CabinCrew> cabinCrew) {
+        this.cabinCrew = cabinCrew;
+    }
+
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+
+    public void setFlightNum(String flightNum) {
+        this.flightNum = flightNum;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public void setGetDepartureTime(String getDepartureTime) {
+        this.getDepartureTime = getDepartureTime;
+    }
+
+    public Pilot getPilot() {
+        return pilot;
+    }
+
+    public ArrayList<CabinCrew> getCabinCrew() {
+        return cabinCrew;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public String getFlightNum() {
+        return flightNum;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getGetDepartureTime() {
+        return getDepartureTime;
+    }
 }
